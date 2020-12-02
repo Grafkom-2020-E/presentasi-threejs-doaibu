0b278728e3b3669f135273ec099085682baf3ed4
Segara Bhagas Dagsapurwa <segara2410@gmail.com>
2020-12-02 10:45:02 +0700

    add mesh normal

---
+++ assets/js/mesh_normal.js
@@ -0,0 +1,89 @@
+let renderer, camera;
+
+const controls = {
+    cameraZ: 5,
+    normalMap: 0,
+    normalScale: 1,
+}
+
+const addGuiMeshBasic = (controls) => {
+    let gui = new dat.GUI();
+    gui.add(controls, 'cameraZ', 0, 10.0);
+    gui.add(controls, 'normalMap', {
+        None: 0,
+        Dissolve: 1,
+        Wood: 2
+    });
+    gui.add(controls, 'normalScale', 0, 1);
+}
+const main = () => {
+
+    let material = new THREE.MeshNormalMaterial();
+    let dissolve_texture = new THREE.TextureLoader().load('assets/textures/dissolve.png');
+    let wood_texture = new THREE.TextureLoader().load('assets/textures/wood.png');
+
+    addGuiMeshBasic(controls);
+
+    renderer = new THREE.WebGLRenderer();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    let scene = new THREE.Scene();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    let geometry = new THREE.SphereGeometry(1, 32, 32);
+    let sphere = new THREE.Mesh( geometry, material );
+    sphere.position.x = -2.5;
+    scene.add( sphere );
+
+    let geometry1 = new THREE.BoxGeometry(1, 1, 1);
+    let box = new THREE.Mesh(geometry1, material);
+    box.position.x = 2.5;
+    scene.add(box);
+
+    let geometry2 = new THREE.PlaneGeometry(10000, 10000, 100, 100);
+    let plane = new THREE.Mesh(geometry2, material);
+    plane.rotation.x = -90 * Math.PI / 180;
+    plane.position.y = -100;
+    scene.add(plane);
+
+    let directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+    directionalLight.position.set(1, 1, 2.6);
+    scene.add( directionalLight );
+    
+    const animate = () => {
+        requestAnimationFrame(animate);
+
+        if (controls.normalMap == 0) {
+            material.normalMap = null;
+        } else if (controls.normalMap == 1) {
+            material.normalMap = dissolve_texture;
+        } else if (controls.normalMap == 2) {
+            material.normalMap = wood_texture;
+        }
+
+        material.needsUpdate = true;
+        material.normalScale = new THREE.Vector2(controls.normalScale, controls.normalScale);
+        camera.position.z = controls.cameraZ;
+
+        rotateMesh(sphere);
+        rotateMesh(box);
+        renderer.render(scene,camera);
+    }
+    animate();
+}
+
+const rotateMesh = (mesh) => {
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01;
+}
+
+const setBasicMaterialOnControl = (material, controls) => {
+    material.color.setHex(controls.color);
+}
+
+const resize = () => {
+    if (!camera) return;
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
